f3c3621f877673e96a6951dd934ebb1c44a361a3
Amy Zhao <amy.rh.zhao@gmail.com>
2020-08-27 10:57:34 -0700

    Interaction
    - Changed the way of interaction (how petal affect petals)
    - Added PursuitBehavior
    - Modified Petal Prefab

---
+++ Assets/Scripts/Flocking/SteeringBehaviors/PursuitBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PursuitBehavior : SteeringBehavior
+{
+    [SerializeField] private string mPursuitFilter;
+    [SerializeField] private float mPursuitDistance;
+
+    public override Vector3 Calculate(Agent agent)
+    {
+        Vector3 force = Vector3.zero;
+
+        var agents = AIWorld.Instance.GetAgents();
+        foreach (var agt in agents)
+        {
+            if (agt != agent // not compare to itself
+                && Vector3.Distance(agt.transform.position, agent.transform.position) < mPursuitDistance // agent in range
+                && agt.CompareTag(mPursuitFilter)) // agent is the type   
+            {
+                force += Vector3.Normalize(agt.transform.position - agent.transform.position) * agent.maxSpeed;
+            }
+        }
+
+        if (force == Vector3.zero)
+        {
+            return Vector3.zero;
+        }
+        return (force - agent.velocity) * mWeight;
+    }
+
+}
